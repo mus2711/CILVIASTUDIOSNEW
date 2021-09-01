e86d2d592a22d91202d361c9610d30a80a25862d
mus2711 <ma819@ic.ac.uk>
2021-09-01 13:00:42 +0100

    shiftadjust

--- carousel.js
+++ carousel.js
@@ -1,31 +1,27 @@
-
-
 const carousel = Object.create(null);
 
 carousel.init = function () {
-    var w = window.innerWidth;
-    var video = document.getElementsByTagName("video")[0];
-    var staticEl = document.getElementById("videodivStatic");
-    var staticC = document.getElementById("staticC");
-    var logo = document.getElementById("logo");
+  var w = window.innerWidth;
+  var video = document.getElementsByTagName("video")[0];
+  var staticEl = document.getElementById("videodivStatic");
+  var staticC = document.getElementById("staticC");
+  var logo = document.getElementById("logo");
 
-    console.log(w);
-    if (w > 576) {
-        logo.style.display = "inline";
-        staticEl.style.display = "none";
-        // video.style.display = "flex";
-        video.height = 486 * (w / 1440);
-        video.width = 486 * (w / 1440);
-    }
-    else {
-        logo.style.display = "none";
-        // 
-        staticEl.style.display = "flex";
-        
-        var staticWidth = 195 * (w / 576);
-        staticC.style.maxWidth = `${staticWidth}px`;
-    
-    }
+  // console.log(w);
+  if (w > 576) {
+    logo.style.display = "inline";
+    staticEl.style.display = "none";
+    // video.style.display = "flex";
+    video.height = 486 * (w / 1440);
+    video.width = 486 * (w / 1440);
+  } else {
+    logo.style.display = "none";
+    //
+    staticEl.style.display = "flex";
+
+    var staticWidth = 195 * (w / 576);
+    staticC.style.maxWidth = `${staticWidth}px`;
+  }
 };
 
 export default Object.freeze(carousel);
--- shift.js
+++ shift.js
@@ -1,71 +1,66 @@
 const shift = Object.create(null);
 
 shift.init = function () {
-    const el = function (id) {
-        return document.getElementById(id);
-    };
-    const firstShift = el("firstImg");
-    const thirdShift = el("thirdImg");
-    const secShift = el("secImg");
-    const fourthShift = el("fourthImg");
-    const un1 = el("shiftone");
-    const un2 = el("shifttwo");
-    const un3 = el("shiftthree");
-    const un4 = el("shiftfour");
-    
-    function shiftItRight() {
-        console.log("movin");
-        firstShift.style.marginLeft = "13vw";
-    }
-    function shiftItLeft() {
-      
-        firstShift.style.marginLeft = "10vw";
-    }
-    function shiftItRightTwo() {
+  const el = function (id) {
+    return document.getElementById(id);
+  };
+  const firstShift = el("firstImg");
+  const thirdShift = el("thirdImg");
+  const secShift = el("secImg");
+  const fourthShift = el("fourthImg");
+  const un1 = el("shiftone");
+  const un2 = el("shifttwo");
+  const un3 = el("shiftthree");
+  const un4 = el("shiftfour");
 
-        thirdShift.style.marginLeft = "13vw";
-    }
-    function shiftItLeftTwo() {
-
-        thirdShift.style.marginLeft = "10vw";
-    }
-    function shiftItLeftThree() {
-        
-        secShift.style.marginRight = "13vw";
-    }
-    function shiftItRightThree() {
-      
-        secShift.style.marginRight = "10vw";
-    }
-    function shiftItLeftFour() {
-
-        fourthShift.style.marginRight = "13vw";
-    }
-    function shiftItRightFour() {
-
-        fourthShift.style.marginRight = "10vw";
-    }
-    var width = window.innerWidth;
-    if (width > 768) {
-        firstShift.addEventListener("mouseover", shiftItRight);
-        un1.addEventListener("mouseover", shiftItRight);
-        firstShift.addEventListener("mouseleave", shiftItLeft);
-        un1.addEventListener("mouseleave", shiftItLeft);
-        thirdShift.addEventListener("mouseover", shiftItRightTwo);
-        un3.addEventListener("mouseover", shiftItRightTwo);
-        thirdShift.addEventListener("mouseleave", shiftItLeftTwo);
-        un3.addEventListener("mouseleave", shiftItLeftTwo);
-        secShift.addEventListener("mouseover", shiftItLeftThree);
-        un2.addEventListener("mouseover", shiftItLeftThree);
-        secShift.addEventListener("mouseleave", shiftItRightThree);
-        un2.addEventListener("mouseleave", shiftItRightThree);
-        fourthShift.addEventListener("mouseover", shiftItLeftFour);
-        un4.addEventListener("mouseover", shiftItLeftFour);
-        fourthShift.addEventListener("mouseleave", shiftItRightFour);
-        un4.addEventListener("mouseleave", shiftItRightFour);
-        // firstShift.addEventListener("mouseover", test);
-        // firstShift.addEventListener("mouseleave", shiftItLeft(firstShift));
-    }
+  function shiftItRight() {
+    // console.log("movin");
+    firstShift.style.marginLeft = "13vw";
+  }
+  function shiftItLeft() {
+    console.log("movin2");
+    firstShift.style.marginLeft = "10vw";
+  }
+  function shiftItRightTwo() {
+    thirdShift.style.marginLeft = "13vw";
+  }
+  function shiftItLeftTwo() {
+    thirdShift.style.marginLeft = "10vw";
+  }
+  function shiftItLeftThree() {
+    console.log("movinsec");
+    secShift.style.marginRight = "11vw";
+  }
+  function shiftItRightThree() {
+    secShift.style.marginRight = "8vw";
+  }
+  function shiftItLeftFour() {
+    fourthShift.style.marginRight = "11vw";
+  }
+  function shiftItRightFour() {
+    fourthShift.style.marginRight = "8vw";
+  }
+  var width = window.innerWidth;
+  if (width > 768) {
+    firstShift.addEventListener("mouseover", shiftItRight);
+    un1.addEventListener("mouseover", shiftItRight);
+    firstShift.addEventListener("mouseleave", shiftItLeft);
+    un1.addEventListener("mouseleave", shiftItLeft);
+    thirdShift.addEventListener("mouseover", shiftItRightTwo);
+    un3.addEventListener("mouseover", shiftItRightTwo);
+    thirdShift.addEventListener("mouseleave", shiftItLeftTwo);
+    un3.addEventListener("mouseleave", shiftItLeftTwo);
+    secShift.addEventListener("mouseover", shiftItLeftThree);
+    un2.addEventListener("mouseover", shiftItLeftThree);
+    secShift.addEventListener("mouseleave", shiftItRightThree);
+    un2.addEventListener("mouseleave", shiftItRightThree);
+    fourthShift.addEventListener("mouseover", shiftItLeftFour);
+    un4.addEventListener("mouseover", shiftItLeftFour);
+    fourthShift.addEventListener("mouseleave", shiftItRightFour);
+    un4.addEventListener("mouseleave", shiftItRightFour);
+    // firstShift.addEventListener("mouseover", test);
+    // firstShift.addEventListener("mouseleave", shiftItLeft(firstShift));
+  }
 };
 
 export default Object.freeze(shift);
